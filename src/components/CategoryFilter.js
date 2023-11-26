2231e868589184c5f2ed35ceeb5003c12cf3ceed
matchy123 <user.email>
2023-11-26 20:35:17 +0300

    Done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+  const [filterTask, setFilterTask] = useState("All")
+  function handleClick(category){
+    
+    setFilterTask(category)
+
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+       categories.map((category, index)=>{
+          return <button classname={filterTask === category?"selected":""} key={index} value={category} onClick={()=>handleClick(category)}>{category}</button>
+        })
+      }
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const [deletedTasks, setDeletedTasks] = useState([]);
+
+  function handleDelete(index) {
+    const updatedDeletedTasks = [...deletedTasks, index];
+    setDeletedTasks(updatedDeletedTasks);
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => {
+        if (!deletedTasks.includes(index)) {
+          return (
+            <Task key={index} text={task.text} category={task.category} handleDelete={() => handleDelete(index)}
+            />
+          );
+        }
+        return null;
+      })}
     </div>
   );
 }
